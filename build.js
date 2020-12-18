7c6e1c1cf36eca07f42daa611270f44045b958e8
zmitry <kraken@live.ru>
2020-12-18 21:37:05 +0200

    add preview

--- build.js
+++ build.js
@@ -1,15 +1,30 @@
 const { build } = require("esbuild");
 
-build({
-  entryPoints: ["./src/index.tsx"],
-  minify: false,
-  define: {
-    "process.env.NODE_ENV": '"production"',
-  },
-  outfile: "./build/index.js",
-  bundle: true,
-  external: ["react"],
-  target: "es2017",
-  format: "esm",
-  sourcemap: "external",
-}).then(console.log, console.error);
+const env = {
+  "process.env.NODE_ENV": '"development"',
+};
+
+if (process.argv.includes("--preview")) {
+  build({
+    entryPoints: ["./preview/index.tsx"],
+    minify: false,
+    define: env,
+    outfile: "./preview-build/index.js",
+    bundle: true,
+    target: "es2015",
+    format: "esm",
+    sourcemap: "inline",
+  }).then(console.log, console.error);
+} else {
+  build({
+    entryPoints: ["./src/index.tsx"],
+    minify: false,
+    define: env,
+    outfile: "./build/index.js",
+    bundle: true,
+    external: ["react"],
+    target: "es2017",
+    format: "esm",
+    sourcemap: "external",
+  }).then(console.log, console.error);
+}
